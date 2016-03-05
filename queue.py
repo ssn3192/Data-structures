cf293d5549584b86a4d6dc761193f2f1679ad48f
Shivani Narang <shivu.narang@gmail.com>
2016-03-04 21:03:06 -0600

    Added files via upload

---
+++ queue.py
@@ -0,0 +1,29 @@
+class Queue:
+    def __init__(self):
+         self.items = []
+    
+    def isEmpty(self):
+        return self.items == []
+    
+    def sizeofqueue(self):
+        return len(self.items)
+    
+    def enqueue(self,item):
+        self.items.insert(0, item)
+        
+    def dequeue(self):
+        return self.items.pop()
+
+
+q= Queue()
+q.enqueue("shivani")
+q.enqueue("1")
+q.enqueue("True")
+print (q.sizeofqueue())
+q.dequeue()
+q.dequeue()
+q.enqueue("narang")
+print (q.isEmpty())
+q.dequeue()
+q.dequeue()
+print(q.isEmpty())            
